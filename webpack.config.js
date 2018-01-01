06fbdfe7636573b635b5af8bf8286a54a443f11f
bobby569 <zhan2175@purdue.edu>
2017-12-31 21:57:50 -0500

    update package & webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: 'build/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
+			}
+		]
+	},
+	plugins: [new ExtractTextPlugin('style.css')],
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+		overlay: true
+	}
 };
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports.devtool = '#source-map';
+	// http://vue-loader.vuejs.org/en/workflow/production.html
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	]);
+}
